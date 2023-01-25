1cddf4fa306b2577a0224310054aba7c78020c28
jd3096-mpy <1091329318@qq.com>
2023-01-25 09:32:18 +0800

    install auto

--- getdata.py
+++ getdata.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+import time
+from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get('https://www.kickstarter.com/projects/xgorobot/xgo-2-worlds-first-raspberry-pi-robotic-dog-with-an-arm?ref=discovery')
 time.sleep(5)
