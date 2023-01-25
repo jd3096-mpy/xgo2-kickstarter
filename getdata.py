3f597b166ecc3cf057ce4cc478b1a8b1888b7074
jd3096-mpy <1091329318@qq.com>
2023-01-25 10:01:46 +0800

    edge

--- getdata.py
+++ getdata.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
 import time
 from selenium import webdriver
-driver = webdriver.Firefox()
+driver = webdriver.Edge()
 driver.get('https://www.kickstarter.com/projects/xgorobot/xgo-2-worlds-first-raspberry-pi-robotic-dog-with-an-arm?ref=discovery')
 time.sleep(5)
