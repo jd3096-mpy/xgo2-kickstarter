fe777e115106a15a51bdc1eeeab37a6088512d6c
jd3096-mpy <1091329318@qq.com>
2023-01-25 10:16:50 +0800

    txt

--- getdata.py
+++ getdata.py
@@ -1,11 +1,26 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
-import time
+time.sleep(1)
+print('driver installed')
+#------------------------------------------
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import re
+
 driver = webdriver.Edge()
 driver.get('https://www.kickstarter.com/projects/xgorobot/xgo-2-worlds-first-raspberry-pi-robotic-dog-with-an-arm?ref=discovery')
 time.sleep(5)
+datas=driver.find_element(By.XPATH, '//*[@id="react-prelaunch-root"]/div/div/div/div/div[2]/div[1]/div[2]/span')
+nums=re.findall("\d+",datas.text) 
+people=nums[0]
+print(people)
+with open('result.txt','w') as f:
+    f.write(people)
+driver.close()
+
---
+++ sstpy.py
@@ -0,0 +1,5 @@
+import re
+str_ = "168 名跟隨者"
+number = re.findall("\d+",str_)    # 输出结果为列表
+print(number)
+ss='168 名跟隨者'
