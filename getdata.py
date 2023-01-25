9d4859b312864847fd8ad8ea54e821a7c9c150e8
jd3096-mpy <1091329318@qq.com>
2023-01-25 09:02:39 +0800

    try

---
+++ getdata.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+driver = webdriver.Firefox()
+driver.get('https://www.kickstarter.com/projects/xgorobot/xgo-2-worlds-first-raspberry-pi-robotic-dog-with-an-arm?ref=discovery')
+time.sleep(5)
