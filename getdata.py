e9e1d1867e4451f38ba317f76cba4f3979c1896f
jd3096-mpy <1091329318@qq.com>
2023-01-30 10:54:10 +0800

    countday

--- getdata.py
+++ getdata.py
@@ -1,12 +1,12 @@
 import time
 
-from selenium import webdriver
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-
-driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
-
-time.sleep(1)
+# from selenium import webdriver
+# from selenium.webdriver.edge.service import Service as EdgeService
+# from webdriver_manager.microsoft import EdgeChromiumDriverManager
+# 
+# driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+# 
+# time.sleep(1)
 print('driver installed')
 #------------------------------------------
 from selenium import webdriver
@@ -15,7 +15,7 @@ import re
 
 driver = webdriver.Edge()
 driver.get('https://www.kickstarter.com/projects/xgorobot/xgo-2-worlds-first-raspberry-pi-robotic-dog-with-an-arm?ref=discovery')
-time.sleep(5)
+time.sleep(50)
 datas=driver.find_element(By.XPATH, '//*[@id="react-prelaunch-root"]/div/div/div/div/div[2]/div[1]/div[2]/span')
 nums=re.findall("\d+",datas.text) 
 people=nums[0]
@@ -27,9 +27,9 @@ import requests
 import json
 headers={"api-key":"VqL9j56HfXisdr8nOvGT=iLl95g="}
 url='http://api.heclouds.com/devices/1039865113/datapoints'
-payload = {'datastreams': [{"id": "people", "datapoints": [{"value": int(people)}]}]}
+payload = {'datastreams': [{"id": "followers", "datapoints": [{"value": int(people)}]}]}
 payload=json.dumps(payload)
 ok=requests.post(headers=headers,url=url,data=payload)
-print(ok)
+print(ok.text)
 driver.close()
 
