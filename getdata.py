b9fe61b07ee9c8a8517458b4a16b503673cc409f
jd3096 <69076780+jd3096-mpy@users.noreply.github.com>
2023-02-01 16:13:48 +0800

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -13,15 +13,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+desired_capabilities = DesiredCapabilities.EDGE
+desired_capabilities["pageLoadStrategy"] = "none"
+
+
 driver = webdriver.Edge()
+print(0)
 driver.get('https://www.kickstarter.com/projects/xgorobot/xgo-2-worlds-first-raspberry-pi-robotic-dog-with-an-arm?ref=discovery')
-time.sleep(50)
-datas=driver.find_element(By.XPATH, '//*[@id="react-prelaunch-root"]/div/div/div/div/div[2]/div[1]/div[2]/span')
-nums=re.findall("\d+",datas.text) 
-people=nums[0]
+print(1)
+time.sleep(10)
+print(2)
+money_data=driver.find_element(By.XPATH, '//*[@id="react-project-header"]/div/div[1]/div[3]/div/div[2]/div[1]/div[2]/span/span')
+people_data=money=driver.find_element(By.XPATH, '//*[@id="react-project-header"]/div/div[1]/div[3]/div/div[2]/div[2]/div/span')
+money=re.findall("\d+",money_data.text) 
+people=re.findall("\d+",people_data.text) 
+people=people[0]
 print(people)
-with open('result.txt','w') as f:
-    f.write(people)
+mm=''
+for m in money:
+    mm+=str(m)
+money=int(mm)
+print(money)
+
 
 import requests
 import json
@@ -31,5 +45,12 @@ payload = {'datastreams': [{"id": "followers", "datapoints": [{"value": int(peop
 payload=json.dumps(payload)
 ok=requests.post(headers=headers,url=url,data=payload)
 print(ok.text)
+
+payload = {'datastreams': [{"id": "moneys", "datapoints": [{"value": int(people)}]}]}
+payload=json.dumps(payload)
+ok=requests.post(headers=headers,url=url,data=payload)
+print(ok.text)
+
 driver.close()
+
 
